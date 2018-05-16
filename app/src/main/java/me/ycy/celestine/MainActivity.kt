3287340a6a38518d392edfacc42c88c01524fb65
Yu Changyuan <reivzy@gmail.com>
2018-05-16 21:06:31 +0800

    Add OpenCV.

--- app/src/main/java/me/ycy/celestine/MainActivity.kt
+++ app/src/main/java/me/ycy/celestine/MainActivity.kt
@@ -2,8 +2,18 @@ package me.ycy.celestine
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import org.opencv.android.OpenCVLoader
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        const val TAG = "celestine"
+        init {
+            if (!OpenCVLoader.initDebug()) {
+                Log.e(TAG, "OpenCV init fail!")
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
